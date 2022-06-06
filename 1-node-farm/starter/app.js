579d3503bb4700d16bb1f507a641e16e4f1a0091
Chloe <cguttridge@clever-touch.com>
2022-06-06 14:56:02 +0100

    creating a simpe api

--- 1-node-farm/starter/app.js
+++ 1-node-farm/starter/app.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const http = require('http')
-
+const url = require('url')
 // Synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt')
 // console.log(textIn)
@@ -25,10 +25,33 @@ const http = require('http')
 //   })
 // })
 
+
 // Simple web server
 
+// top level is only run once
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
+
 const server = http.createServer((req, res) => { 
-  res.end('Hello from server')
+  const pathName = req.url
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is overview')
+  } else if (pathName === '/product'){
+    res.end('This is the product')
+  } else if (pathName === '/api'){
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(data)
+  } else {
+    res.writeHead(404, {
+      // set headers - info about response being sent back
+      'Content-Type': 'text/html',
+      'my-header': 'hello-node'
+    })
+    res.end('<h1>Page not found</h1>')
+  }
+
+
 })
 // 1st parameter is port number using 8000/2nd parameter is the host
 server.listen(8000, '127.0.0.1', () => {
