e2d4d9527beaddc2138c8f8156a8d59dd87efd4a
Chloe <cguttridge@clever-touch.com>
2022-06-07 14:34:49 +0100

    creating templates and populating placeholders with code

--- 1-node-farm/starter/app.js
+++ 1-node-farm/starter/app.js
@@ -30,15 +30,39 @@ const url = require('url')
 
 // top level is only run once
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const templateOverview = fs.readFileSync(`${__dirname}/templates/overview-template.html`, 'utf-8')
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const templateProduct = fs.readFileSync(`${__dirname}/templates/product-template.html`, 'utf-8')
 const dataObj = JSON.parse(data)
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%ID%}/g, product.id)
+
+  if (!product.organic) output.replace(/{%NOT_ORGANIC%}/g, 'not organic')
+    return output
+}
 
 const server = http.createServer((req, res) => { 
   const pathName = req.url
 
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is overview')
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(templateCard, el)).join('')
+    const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml) 
+    res.end(output)
+
+    // Product pages
   } else if (pathName === '/product'){
     res.end('This is the product')
+
+    // API
   } else if (pathName === '/api'){
     res.writeHead(200, { 'Content-Type': 'application/json' })
     res.end(data)
